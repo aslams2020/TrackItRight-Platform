8a185ab18b34ab4c5e0ea1d2dc9ad08ef9907f3f
Sayyad Aslam <sayyadaslam2020@gmail.com>
2025-08-27 02:31:33 +0530

    Create api/axios.js

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import { getToken } from '../utils/auth.js';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080',
+});
+
+api.interceptors.request.use((config) => {
+  const token = getToken();
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default api;
