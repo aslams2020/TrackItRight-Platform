7b987d3bd4cc40d6c5ab98d529b2911f1c52affb
Sayyad Aslam <sayyadaslam2020@gmail.com>
2025-08-27 02:35:36 +0530

    Create AdminDashboard

---
+++ frontend/src/pages/AdminDashboard.jsx
@@ -0,0 +1,8 @@
+export default function AdminDashboard() {
+  return (
+    <div className="page-center">
+      <h2>Admin Dashboard</h2>
+      <p>Users, departments & analytics will be here.</p>
+    </div>
+  );
+}
