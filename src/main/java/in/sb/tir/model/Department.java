53932391c1edb1cac22beabcbed3ca664e6ed2ea
Sayyad Aslam <sayyadaslam2020@gmail.com>
2025-08-28 13:57:06 +0530

    Update Department.java

--- src/main/java/in/sb/tir/model/Department.java
+++ src/main/java/in/sb/tir/model/Department.java
@@ -12,18 +12,24 @@ public class Department {
 
     @Column(nullable = false, unique = true)
     private String name;
-
+    
+    @Column(length = 1000)
+    private String description;
+    
     public Department() {}
 
-    public Department(Long id, String name) {
+    public Department(Long id, String name, String description) {
         this.id = id;
         this.name = name;
+        this.description = description;
     }
-
     // Getters and Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+    
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
 }
