86226a7ee15139de29651c9592edaed6aabd7738
Sayyad Aslam <sayyadaslam2020@gmail.com>
2025-08-28 15:41:38 +0530

    update footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,8 +5,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>© {new Date().getFullYear()} TrackItRight. All rights reserved.</p>
-      <p>Made with⚡by Aslam</p>
+      <p>© {new Date().getFullYear()} TrackItRight. All rights reserved | Made with⚡by Aslam</p>
       <p className="footer-links">
         <a href="https://www.linkedin.com/in/aslamsayyad02/">LinkedIn</a> |{" "}
         <a href="https://github.com/aslams2020">GitHub</a>
