aec62edf89d08379085b05084d313de2a2d9de75
Sayyad Aslam <sayyadaslam2020@gmail.com>
2025-08-28 14:14:34 +0530

    Update Complaintrequest test

--- src/main/java/in/sb/tir/dto/ComplaintRequest.java
+++ src/main/java/in/sb/tir/dto/ComplaintRequest.java
@@ -6,12 +6,24 @@ public class ComplaintRequest {
     private Long departmentId;
 
     // getters & setters
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
+    public String getTitle() { 
+    	return title; 
+    }
+    public void setTitle(String title) {
+    	this.title = title; 
+    }
 
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
+    public String getDescription() {
+    	return description; 
+    }
+    public void setDescription(String description) {
+    	this.description = description; 
+    }
 
-    public Long getDepartmentId() { return departmentId; }
-    public void setDepartmentId(Long departmentId) { this.departmentId = departmentId; }
+    public Long getDepartmentId() { 
+    	return departmentId; 
+    }
+    public void setDepartmentId(Long departmentId) { 
+    	this.departmentId = departmentId; 
+    }
 }
