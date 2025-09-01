f862e19b9010925d849e513916b02b70a71fe764
Sayyad Aslam <sayyadaslam2020@gmail.com>
2025-09-01 11:48:20 +0530

    fixed login

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../api/axios';
 import { setAuth } from '../utils/auth';
-import './AuthPages.css';
+import '../styles/AuthPages.css';
 import { toast } from '../utils/toast'; 
 
 export default function LoginPage() {
